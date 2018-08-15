07591d5a627752bd413ad0d5718309f8f45035e8
Andy Gertjejansen <andy.gertjejansen@bestbuy.com>
2018-08-14 20:36:57 -0500

    Commit progress

--- app/builder.go
+++ app/builder.go
@@ -1,24 +1,54 @@
 package app
 
 import (
+	"github.com/aufaitio/data-access"
+	"github.com/aufaitio/data-access/daos"
+	"github.com/aufaitio/data-access/models"
 	"github.com/aufaitio/plugins/lib/logger"
 	"github.com/mongodb/mongo-go-driver/mongo"
 )
 
 // Builder checks build queue and builds
 type Builder struct {
-	db     *mongo.Database
-	logger logger.Logger
+	db      *mongo.Database
+	logger  logger.Logger
+	jobDAO  access.JobDAO
+	repoDAO access.RepositoryDAO
 }
 
 // NewBuilder factory for builder
 func NewBuilder(db *mongo.Database, logger logger.Logger) *Builder {
-	return &Builder{db, logger}
+	jobDAO := daos.NewJobDAO()
+	repoDAO := daos.NewRepositoryDAO()
+
+	return &Builder{db, logger, jobDAO, repoDAO}
 }
 
 // Check check build queue for build
-func (builder *Builder) Check() {
+func (builder *Builder) Check() error {
 	builder.logger.Infof("Checking build queue")
+	jobList, err := builder.jobDAO.Query(builder.db, 0, 1)
+
+	if err != nil {
+		return err
+	}
+
+	if len(jobList) > 0 {
+		job := jobList[0]
+		repo, err := builder.repoDAO.GetByName(builder.db, job.Name)
+
+		if err != nil {
+			return err
+		}
+
+		err = builder.Spawn(job, repo)
+	}
+
+	return err
+}
+
+// Spawn spawns a build from queue
+func (builder *Builder) Spawn(job *models.Job, repo *models.Repository) error {
 }
 
 // CleanUp Cleans up builder job
