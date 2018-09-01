942ace4be9fb24feb6544e193dced947ce24d007
Andy Gertjejansen <andy.gertjejansen@bestbuy.com>
2018-09-01 12:43:59 -0500

    Build basic builder structure

--- app/builder.go
+++ app/builder.go
@@ -7,6 +7,9 @@ import (
 	"github.com/quantumew/data-access/daos"
 	"github.com/quantumew/data-access/models"
 	"github.com/quantumew/plugins/lib/logger"
+	"gopkg.in/src-d/go-git.v4"
+	"os"
+	"os/exec"
 )
 
 // Builder checks build queue and builds
@@ -28,31 +31,71 @@ func NewBuilder(db *mongo.Database, logger logger.Logger) *Builder {
 // Check check build queue for build
 func (builder *Builder) Check() {
 	builder.logger.Infof("Checking build queue")
-	jobList, err := builder.jobDAO.Query(builder.db, 0, 1)
+	job, err := builder.jobDAO.Claim(builder.db)
 
 	if err != nil {
 		panic("failed to query for job")
 	}
 
-	if len(jobList) > 0 {
-		job := jobList[0]
-		repo, err := builder.repoDAO.Get(builder.db, job.Name)
+	repo, err := builder.repoDAO.Get(builder.db, job.Name)
 
-		if err != nil {
-			panic(fmt.Sprintf("failed to get repository for given job %s", job.Name))
-		}
+	if err != nil {
+		builder.jobDAO.Release(builder.db, job)
+		panic(fmt.Sprintf("failed to get repository for given job %s", job.Name))
+	}
 
-		err = builder.Spawn(job, repo)
+	err = builder.Spawn(job, repo)
 
-		if err != nil {
-			panic(fmt.Sprintf(`failed to spawn job "%s" for repository "%s"`, job.Name, repo.Name))
-		}
+	if err != nil {
+		builder.jobDAO.Release(builder.db, job)
+		panic(fmt.Sprintf(`failed to spawn job "%s" for repository "%s"`, job.Name, repo.Name))
 	}
 }
 
 // Spawn spawns a build from queue
 func (builder *Builder) Spawn(job *models.Job, repo *models.Repository) error {
-	return nil
+	// Clone, update, install, healthcheck, push, open pull request
+	dir := os.TempDir()
+	_, err := git.PlainClone(dir, false, &git.CloneOptions{
+		URL:      repo.Config.Remote,
+		Progress: os.Stdout,
+	})
+
+	// npm will respect the semver version, so npm update depOne depTwo ...
+	// perhaps we should provide the ability to override via .aufait.yaml...
+	// Also perhaps a --registry option
+	argList := []string{"update"}
+
+	for _, dep := range job.Dependencies {
+		argList = append(argList, dep.Name)
+	}
+	err = builder.runNpmCommand(dir, argList)
+
+	if err != nil {
+		return fmt.Errorf("Failed to run npm update, reason %s", err.Error())
+	}
+
+	// Ability to do lock only when a healthcheck is disabled
+	err = builder.runNpmCommand(dir, []string{"install"})
+
+	if err != nil {
+		return fmt.Errorf("Failed to run npm install, reason %s", err.Error())
+	}
+
+	err = builder.runNpmCommand(dir, []string{"test"})
+
+	// Open failure PR
+	if err != nil {
+	} else {
+	}
+
+	return err
+}
+
+func (builder *Builder) runNpmCommand(cwd string, argList []string) error {
+	cmd := exec.Command("npm", argList...)
+	cmd.Path = cwd
+	return cmd.Run()
 }
 
 // CleanUp Cleans up builder job
